723db97b1d8bb3550219420ecb10d13dff2d80df
Affan Muhammad Ghazali <affanmuhammad@pk.folio3.com>
2022-07-14 11:53:33 +0500

    POST method done for articles

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -6,4 +6,14 @@ class ArticlesController < ApplicationController
   def index
     @articles = Article.all
   end
+
+  def new
+  end
+
+  def create
+    @articles = Article.new(params.require(:article).permit(:title, :description))
+    @articles.save
+    redirect_to article_path(@articles)
+  end
+
 end
